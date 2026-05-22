6c149ba441b88a1b04b692bf6718d9119371cdd7
SamiraAlimova <s25174200@gmail.com>
2026-05-22 11:39:43 -0400

    Update Part A.java

--- question-1/Part A.java
+++ question-1/Part A.java
@@ -1,1 +1,13 @@
-// Solu
+public Account( String requestedName){
+    int num =1;
+    if(isAvailable(requestedName)){
+        username = requestedName;
+    }else{
+      while(!isAvailable(requestedName+num)){
+       
+        num++;
+      }
+     username = requestedName+num;  
+    } 
+    
+}
